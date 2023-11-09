a1fe4da9d2e18ca284b15b49074d0bc0cc1d8ad8
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2023-11-09 11:05:26 +0000

    Setup advancade branch structure

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,5 @@
 """The root of the project with the hydra entry point.
 
-All logic should flow through hydra.
-Type everything using interfaces and ABCs rather than concretions.
+All logic should flow through hydra. Type everything using interfaces and ABCs rather
+than concretions.
 """
