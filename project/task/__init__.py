83a6a196c7c5263b971ec01005035cb278756121
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2023-11-09 12:58:57 +0000

    Create config file structure

--- project/task/__init__.py
+++ project/task/__init__.py
@@ -1,6 +1,5 @@
 """Task code including datasets, models and train/test loops.
 
-The default task contains functionality likely to generalise across tasks. Copy it for
-adding a new task, but do not duplicate functionality like a simple training function
-for images.
+The default task serves as a template and may contain functionality likely to generalise
+across tasks. Copy it for adding a new task.
 """
