53ce91108035807a8a820c9a6593f00d421eceec
Preslav Aleksandrov <pa511@cam.ac.uk>
2024-01-25 17:37:18 +0000

    further standardise poetry and pyenv locations on disk. imporve script when installing after new hom

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-VPOETRY_HOME=""
-VPYENV_ROOT=""
+USER_NAME=$(echo $USER)
+VPOETRY_HOME="/nfs-share/$USER_NAME/.poetry"
+VPYENV_ROOT="/nfs-share/$USER_NAME/.pyenv"
 VPYTHON_VERSION="3.11.6"
-
 
 if ! [ -x "$(command -v pyenv)" ]; then
   if [ -z "$VPYENV_ROOT" ]; then
@@ -13,15 +12,17 @@ if ! [ -x "$(command -v pyenv)" ]; then
     echo "PYENV_ROOT is not empty"
   fi
 
-
   curl https://pyenv.run | bash
-  eval "$(PYENV_ROOT=$VPYENV_ROOT pyenv init -)"
-  echo "export PYENV_ROOT=\"$VPYENV_ROOT\"" >> ~/.bashrc
-  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-  echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-  echo "export PYENV_ROOT=\"$VPYENV_ROOT\"" >> ~/.profile
-  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-  echo 'eval "$(pyenv init -)"' >> ~/.profile
+  PYENV_ROOT=$VPYENV_ROOT
+  PYENV_BIN=$PYENV_ROOT/bin/pyenv
+
+  eval "$($PYENV_BIN init -)"
+  echo "export PYENV_ROOT=\"$VPYENV_ROOT\"" >> /home/$USER_NAME/.bashrc
+  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/$USER_NAME/.bashrc
+  echo 'eval "$(pyenv init -)"' >> /home/$USER_NAME/.bashrc
+  echo "export PYENV_ROOT=\"$VPYENV_ROOT\"" >> /home/$USER_NAME/.profile
+  echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> /home/$USER_NAME/.profile
+  echo 'eval "$(pyenv init -)"' >> /home/$USER_NAME/.profile
 else
   echo "Pyenv is already installed"
 fi
@@ -37,33 +38,36 @@ if [ -z "$(command -v poetry)" ]; then
 
   mkdir -p $VPOETRY_HOME
   curl -sSL https://install.python-poetry.org | POETRY_HOME=$VPOETRY_HOME python3 -
+  
+  POETRY_BIN="$VPOETRY_HOME/bin/poetry"
+
+  echo 'command -v poetry >/dev/null || export PATH="'$VPOETRY_HOME'/bin:$PATH"' >> /home/$USER_NAME/.bashrc
+  echo 'command -v poetry >/dev/null || export PATH="'$VPOETRY_HOME'/bin:$PATH"' >> /home/$USER_NAME/.profile
+
 else
   echo "Poetry is already installed"
 fi
 
-
-
-if pyenv versions | grep -q $VPYTHON_VERSION; then
+if $PYENV_BIN versions | grep -q $VPYTHON_VERSION; then
   echo "Python $VPYTHON_VERSION is already installed"
 else
-  pyenv install $VPYTHON_VERSION
+  $PYENV_BIN install $VPYTHON_VERSION
 fi
 
 
-current_version=$(pyenv local)
+current_version=$($PYENV_BIN local)
 
 if [ "$current_version" = "$VPYTHON_VERSION" ]; then
   echo "The local Python version is already set to $VPYTHON_VERSION"
 else
-  pyenv local $VPYTHON_VERSION
+  $PYENV_BIN local $VPYTHON_VERSION
 fi
-poetry env use $VPYTHON_VERSION
+$POETRY_BIN env use $VPYTHON_VERSION
 
-poetry install
+$POETRY_BIN install
 
 # Install pre-commit hooks
-poetry run pre-commit install
-
+$POETRY_BIN run pre-commit install
 
 # Command to run all pre-commit hooks
-poetry run pre-commit run --all-files --hook-stage push
+$POETRY_BIN run pre-commit run --all-files --hook-stage push
